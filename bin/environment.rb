4f90809965504d68adae245d95ef276eca8268eb
David Crosby <dcrosby@glscrip.com>
2020-09-22 21:51:33 -0400

    Bundler.setup in environment

--- bin/environment.rb
+++ bin/environment.rb
@@ -1,7 +1,10 @@
-$PROJECT_ROOT = File.expand_path(File.join(__dir__, ".."))
-$LOAD_PATH << File.join($PROJECT_ROOT, "lib")
+$PROJECT_ROOT = File.expand_path(File.join(__dir__, '..'))
+$LOAD_PATH << File.join($PROJECT_ROOT, 'lib')
 
-require "time"
-require "attr_extras"
-require "pry"
-require "active_support/core_ext/numeric/time"
+require 'bundler'
+Bundler.setup
+
+require 'time'
+require 'attr_extras'
+require 'pry'
+require 'active_support/core_ext/numeric/time'
